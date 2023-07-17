bb828044982f6f69658cbb7624bc4d36607d0262
Nikhil Kumar <vashishtnikhil158@gmail.com>
2023-07-17 18:41:29 +0530

    average_age

---
+++ SPRINT-4/DAY-1/Eval/average.py
@@ -0,0 +1,25 @@
+company = {
+    'employees': {
+        'John': {'age': 35, 'job_title': 'Manager'},
+        'Emma': {'age': 28, 'job_title': 'Software Engineer'},
+        'Kelly': {'age': 41, 'job_title': 'Senior Developer'},
+        'Sam': {'age': 30, 'job_title': 'Software Engineer'},
+        'Mark': {'age': 37, 'job_title': 'Senior Manager'},
+        'Sara': {'age': 32, 'job_title': 'Software Engineer'},
+    }
+}
+
+# print(average_age_of_employees_with_s_job_title(company))  # 31.0
+
+
+
+
+
+
+def average_age(company):
+    # print(company)
+    age=0
+    age+=company.employees
+    print(age)
+  
+average_age(company);    
